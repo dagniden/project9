e63b9f8d7a011acc88fc865f949876279629dc77
Daniil Gnidenko <dagnidenko@gmail.com>
2025-06-30 20:21:40 +0300

    финальные правки перед сдачей ДЗ

--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,26 @@
-"""Модуль содержит функции для работы с новыми возможностями приложения"""
+"""Модуль содержит функции для работы с виджетом"""
 
 from datetime import datetime
 
 from src import masks
 
 
-def mask_account_card(account_card: str) -> str:
-    """Возвращает замаскированный номер счета или номер карты"""
-    numbers_list = [x for x in account_card if x.isdigit()]
-    numbers = int("".join(numbers_list))
-    prefix = "".join([x for x in account_card if x.isalpha() or x == " "])
+def mask_account_card(account_string: str) -> str:
+    """Принимает номер счета или номер карты и возвращает замаскированный номер счета или номер карты"""
+    account_digits_list = [x for x in account_string if x.isdigit()]
+    account_digits = int("".join(account_digits_list))
+    account_prefix = "".join([x for x in account_string if x.isalpha() or x == " "])
 
-    if "Счет" in prefix:
-        masked_account = masks.get_mask_account(numbers)
-        return f"{prefix} {masked_account}"
+    if "Счет" in account_prefix:
+        masked_account = masks.get_mask_account(account_digits)
+        return f"{account_prefix} {masked_account}"
     else:
-        masked_card = masks.get_mask_card_number(numbers)
-        return f"{prefix} {masked_card}"
+        masked_card = masks.get_mask_card_number(account_digits)
+        return f"{account_prefix} {masked_card}"
 
 
 def get_date(date_time: str) -> str:
-    """"""
+    """На вход принимает строку с датой в ISO формате и возвращает строку в формате ДД.ММ.ГГГГ"""
     dt = datetime.fromisoformat(date_time)
     return dt.strftime("%d.%m.%Y")
 
