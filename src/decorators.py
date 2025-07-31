bc248f47d8d38959a35f286550aa5d1910198588
Daniil Gnidenko <dagnidenko@gmail.com>
2025-07-31 15:17:41 +0300

    Добавлена запись лога в файл

--- src/decorators.py
+++ src/decorators.py
@@ -1,30 +1,46 @@
+import os
 from functools import wraps
 
 
-def log(filename=None):
+def log(filename: str = ""):
     def decorator(func):
         @wraps(func)
-        def wrapper(*args):
-
+        def wrapper(*args, **kwargs):
             try:
-                result = func(*args)
-                if filename == None:
-                    print(f"{func.__name__} ok")
-
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok"
+                if filename is "":
+                    print(log_message)
+                else:
+                    write_log(filename, log_message)
                 return result
-            except Exception:
-                if filename == None:
-                    print(f"{func.__name__} error")
-            raise Exception
+            except Exception as e:
+                log_message = f"{func.__name__} error: {type(e)}. Inputs: {args}, {kwargs}"
+                if filename is "":
+                    print(log_message)
+                else:
+                    write_log(filename, log_message)
+                raise
 
         return wrapper
 
     return decorator
 
 
-if __name__ == "__main__":
-    @log("1.txt")
-    def my_function(x, y):
-        return x + y
+def write_log(filename: str, message: str) -> None:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(current_dir, "..", "data")
+    file_path = os.path.join(data_dir, filename)
+    os.makedirs(data_dir, exist_ok=True)
+    with open(file_path, mode="a", encoding="UTF-8") as file:
+        file.write(message + "\n")
 
-    my_function(1, 2)
+
+if __name__ == "__main__":
+    @log()
+    def my_function(x, y):
+        # return x + y
+        raise ValueError("ERROR")
+
+
+    my_function("abc", 5)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,7 +1,7 @@
 from src.decorators import log
 
 
-def test_log(capsys):
+def test_log_console(capsys) -> None:
 
     # Проверка вызова декоратора без параметра
     @log()
@@ -11,3 +11,17 @@ def test_log(capsys):
     result = my_function(1, 2)
     captured = capsys.readouterr()
     assert captured.out == "my_function ok\n"
+
+
+def test_log_file() -> None:
+    filename = "data/log.txt"
+
+    # Проверка вызова декоратора без параметра
+    @log(filename)
+    def my_test_function(x, y):
+        return x + y
+
+    with open(filename, mode="r") as file:
+        data = file.read()
+
+    assert "my_test_func ok" in data
