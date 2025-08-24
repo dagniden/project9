9d5e7f4373f03d2aad90d9a5a9ce2a2bef6992e0
Daniil Gnidenko <dagnidenko@gmail.com>
2025-08-24 19:03:19 +0300

    Добавлен логирование для декоратора log через logging

--- src/decorators.py
+++ src/decorators.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from datetime import datetime
 from functools import wraps
@@ -7,29 +8,43 @@ P = ParamSpec("P")  # параметры исходной функции
 R = TypeVar("R")  # возвращаемое значение исходной функции
 
 
+def get_logger(log_filename: str) -> logging.Logger:
+    """Создает и настраивает отдельный логгер для файла"""
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    log_dir = os.path.join(current_dir, "..", "logs")
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_file = os.path.join(log_dir, log_filename)
+
+    logger = logging.getLogger(log_filename)  # уникальное имя = имя файла
+    logger.setLevel(logging.DEBUG)
+
+    # чтобы не дублировались хендлеры при многократном вызове
+    if not logger.handlers:
+        file_handler = logging.FileHandler(log_file, encoding="utf-8", mode="a+")
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
 def log(filename: str = "") -> Callable[[Callable[P, R]], Callable[P, R]]:
     """Логирует начало и конец выполнения функции, а также ее результаты или возникшие ошибки"""
+    logger = get_logger(filename)
 
     def decorator(func: Callable[P, R]) -> Callable[P, R]:
         @wraps(func)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            start = get_current_time()
+        def wrapper(*args, **kwargs) -> R:
+            start_msg = f"Start {func.__name__} with args={args}, kwargs={kwargs}"
+            logger.info(start_msg)
+
             try:
                 result = func(*args, **kwargs)
-                stop = get_current_time()
-                log_message = f"{start=} {stop=} {func.__name__} ok"
-                if filename == "":
-                    print(log_message)
-                else:
-                    write_log(filename, log_message)
+                logger.info(f"End {func.__name__} → result={result}")
                 return result
             except Exception as e:
-                stop = get_current_time()
-                log_message = f"{start=} {stop=} {func.__name__} error: {type(e)}. Inputs: {args}, {kwargs}"
-                if filename == "":
-                    print(log_message)
-                else:
-                    write_log(filename, log_message)
+                logger.exception(f"Error in {func.__name__}")
                 raise
 
         return wrapper
